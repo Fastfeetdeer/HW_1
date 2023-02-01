4bb5604251a1f201fcb8003fba68da60b941b917
e.demin <demsq@mail.ru>
2023-02-01 22:04:56 +0300

    Complete Task 1.4

--- HWT_4/Program.cs
+++ HWT_4/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// ДЗ 1.4
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Ввидите число: ");
+int N = int.Parse(Console.ReadLine());
+var r = 1;
+while (r <= N)
+{
+    if (r % 2 == 0)
+        {
+            Console.Write(r + ", ");
+            r++;
+                    }
+    else
+        {
+            r++;
+        }
+}
